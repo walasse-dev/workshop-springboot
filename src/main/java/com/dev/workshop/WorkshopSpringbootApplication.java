636bb577d5759a971d0d2a20eb8a0e5a4e265553
Walasse Costa <Walasse.Costa0725@jala.university>
2026-08-29 13:49:33 -0300

    Initial commit

---
+++ src/main/java/com/dev/workshop/WorkshopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dev.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/workshop/WorkshopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
