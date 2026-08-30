7bf62c147be44b2cd7c44186d6e9e36c0a75d392
Walasse Costa <Walasse.Costa0725@jala.university>
2026-08-30 10:46:54 -0300

    feat: implements the insert method (POST)

--- src/main/java/com/dev/workshop/controllers/UserController.java
+++ src/main/java/com/dev/workshop/controllers/UserController.java
@@ -3,11 +3,10 @@ package com.dev.workshop.controllers;
 import com.dev.workshop.entities.User;
 import com.dev.workshop.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,5 +26,13 @@ public class UserController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
         return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        User obj = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
